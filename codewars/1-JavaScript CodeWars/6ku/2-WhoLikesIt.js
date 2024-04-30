9c0a71f245111a56dc1141e220fac0ba3b4c85ad
Shawn <sgiovanni1@yahoo.com>
2024-04-30 06:51:27 -0400

    issues with who lieks it

--- codewars/1-JavaScript CodeWars/6ku/2-WhoLikesIt.js
+++ codewars/1-JavaScript CodeWars/6ku/2-WhoLikesIt.js
@@ -4,15 +4,29 @@
 
 //Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
 
-function likes(names) {
+let who = function(callback){
+    //return callback() + "Likes this"
+    callback()
+    console.log( 'Likes this')
+}
 
-  }
+let likes = function(names) {
+    if(!names.length) console.log("no one")
+    if(names.length === 1) console.log(names[0])
+
+    if(names.length < 4){
+        for (let i = 0; i < names.length; i++) {
+            if(i === names.length -1) console.log(`and ${names[i]}` )
+            console.log(`${names[i]} and`)
+        }
+    }
+}
 
 
 
 
-console.log(likes([]))                                -->  //"no one likes this"
-console.log(likes(["Peter"]))                         -->  //"Peter likes this"
-console.log(likes(["Jacob", "Alex"]))                 -->  //"Jacob and Alex like this"
-console.log(likes(["Max", "John", "Mark"]))           -->  //"Max, John and Mark like this"
-console.log(likes(["Alex", "Jacob", "Mark", "Max"]))  -->  //"Alex, Jacob and 2 others like this"
+who(likes([]))                               //"no one likes this"
+console.log(who(likes(["Peter"])))                          //"Peter likes this"
+console.log(who(likes(["Jacob", "Alex"])))                  //"Jacob and Alex like this"
+console.log(who(likes(["Max", "John", "Mark"])))            //"Max, John and Mark like this"
+console.log(who(likes(["Alex", "Jacob", "Mark", "Max"])))   //"Alex, Jacob and 2 others like this"

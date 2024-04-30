3b56bdd84f4f38d7af5b45d21e33aae0e74a44eb
Shawn <sgiovanni1@yahoo.com>
2024-04-30 06:13:14 -0400

    added 6ku   who likes it

---
+++ codewars/1-JavaScript CodeWars/6ku/2-WhoLikesIt.js
@@ -0,0 +1,18 @@
+//6ku WHO LIKES IT
+
+//You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+//Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+function likes(names) {
+
+  }
+
+
+
+
+console.log(likes([]))                                -->  //"no one likes this"
+console.log(likes(["Peter"]))                         -->  //"Peter likes this"
+console.log(likes(["Jacob", "Alex"]))                 -->  //"Jacob and Alex like this"
+console.log(likes(["Max", "John", "Mark"]))           -->  //"Max, John and Mark like this"
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]))  -->  //"Alex, Jacob and 2 others like this"

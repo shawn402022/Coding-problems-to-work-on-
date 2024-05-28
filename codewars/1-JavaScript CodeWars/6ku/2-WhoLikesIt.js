7375dcd4ac1b90a3d59bdd03bfb3483c6f35d77a
Shawn <sgiovanni1@yahoo.com>
2024-05-28 05:43:00 -0400

    working on 6 kyu valid braces

--- codewars/1-JavaScript CodeWars/6ku/2-WhoLikesIt.js
+++ codewars/1-JavaScript CodeWars/6ku/2-WhoLikesIt.js
@@ -4,28 +4,14 @@
 
 //Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
 
-let who = function(){
-    //return callback() + "Likes this"
-    return ' likes this'
-}
+function likes(names){
 
-let likes = function(names, cb) {
-    if(!names.length) console.log("no one" + cb())
-    if(names.length === 1) console.log(names[0] + cb())
-
-    if(names.length < 4){
-        for (let i = 0; i < names.length; i++) {
-            if(i === names.length -1) console.log(`and ${names[i]}` + cb() )
-            console.log(`${names[i]} and` + cb())
-        }
-    }
 }
 
 
 
-
-console.log(likes([], who))                              //"no one likes this"
-console.log(likes(["Peter"], who))                          //"Peter likes this"
-console.log(likes(["Jacob", "Alex"],who))                  //"Jacob and Alex like this"
-console.log(likes(["Max", "John", "Mark"],who))          //"Max, John and Mark like this"
-//console.log(likes(["Alex", "Jacob", "Mark", "Max"],who))   //"Alex, Jacob and 2 others like this"
+console.log(likes([]))                                //"no one likes this"
+console.log(likes(["Peter"]))                         //"Peter likes this"
+console.log(likes(["Jacob", "Alex"]))                  //"Jacob and Alex like this"
+console.log(likes(["Max", "John", "Mark"]))            //"Max, John and Mark like this"
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]))   //"Alex, Jacob and 2 others like this"
---
+++ codewars/1-JavaScript CodeWars/6ku/3-ValudBraces.js
@@ -0,0 +1,43 @@
+/**
+ * 6kyu
+ * VALID BRACES
+ *
+ * Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+What is considered Valid?
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples
+"(){}[]"   =>  True
+"([{}])"   =>  True
+"(}"       =>  False
+"[(])"     =>  False
+"[({})](]" =>  False
+ */
+
+function validBraces(braces){
+
+    let midPoint = braces.length / 2;
+    let curly = '{}'
+    let bracket = '[]'
+    let paren = '()'
+    if(braces.length % 2 === 0) {
+        if(braces[midPoint -1] === curly[0] && braces[midPoint] === curly[1]) return true
+        if(braces[midPoint -1] === bracket[0] && braces[midPoint] === bracket[1]) return true
+        if(braces[midPoint -1] === paren[0] && braces[midPoint] === paren[1]) return true
+
+    } return false;
+}
+
+console.log(validBraces("({})[({})]")) //  True
+console.log(validBraces("()))")) //  False
+console.log(validBraces("()))")) //  False
+console.log(validBraces("(){}[]"))   //  True
+console.log(validBraces("([{}])"))   //  True
+console.log(validBraces("(}"))       //  False
+console.log(validBraces("[(])"))     //  False
+console.log(validBraces("[({})](]")) //  False

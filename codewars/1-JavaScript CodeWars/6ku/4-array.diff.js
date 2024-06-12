80b7bffb9597f16edb1e3ed38ab6982668d4dd77
Shawn <sgiovanni1@yahoo.com>
2024-06-12 07:01:00 -0400

    started arrdiff  6ku

---
+++ codewars/1-JavaScript CodeWars/6ku/4-array.diff.js
@@ -0,0 +1,60 @@
+/**
+ * 6KYU
+ * array.diff
+ *
+ * DESCRIPTION:
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+ */
+
+
+function arrayDiff(a,b){
+    //create copy of the a array so that the length does not change while looping
+    let aCopy = a.slice()
+
+    //create counter to keep track of b elements
+    let bCounter = b.length;
+    let aCounter = aCopy.length;
+
+    //create while  loop.
+    debugger
+
+    while(bCounter !== 0){
+        for (let i = 0; i < aCopy.length; i++) {
+            //remove elements an a that are equal to element in b
+            debugger
+            if (aCopy[i] === b[bCounter-1]) {
+                debugger
+               a.splice(aCopy[i] -1, 1)
+            }
+        }
+        debugger
+        bCounter--
+    }
+    // return a
+    return a
+}
+
+console.log(arrayDiff([9,-7,-4],[9])) // [-7,-4]
+console.log(arrayDiff([6,-10],[6])) // [-10]
+console.log(arrayDiff([14,-6,-10,-12,4,3,3],[14,-6])) // [-10,-12,4,3,3]
+
+console.log(arrayDiff([1,2],[1])) // [2]
+console.log(arrayDiff([1,2,2,2,3],[2])) // [1,3]
+console.log(arrayDiff([1,2,3],[1,2])) // [3]
+
+
+
+
+
+/**
+ * first solution
+ *
+ *
+ */
